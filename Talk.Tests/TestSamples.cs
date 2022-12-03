8d5bc70251a543b1a02e4805919dac003cab0198
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-12-03 08:02:36 -0700

    F!! Samples

--- Talk.Tests/TestSamples.cs
+++ Talk.Tests/TestSamples.cs
@@ -16,11 +16,16 @@ namespace Talk.Tests
         public void TestSimpleSlice()
         {
             var loan1 = new Mock<Loan>(MockBehavior.Strict);
+            loan1.Setup(l => l.IsOverDue()).Returns(true).AtMost(2);
+            loan1.Setup(l => l.GetAmount()).Returns(51).AtMost(2);
+            loan1.Setup(l => l.SaveStatus("Escalated", null)).AtMost(1);
 
-            loan1.Setup(l => l.IsOverDue()).Returns(true);
-            loan1.Setup(l => l.GetAmount()).Returns(5);
+            var loan2 = new Mock<Loan>(MockBehavior.Strict);
+            loan2.Setup(l => l.IsOverDue()).Returns(true).AtMost(1);
+            loan2.Setup(l => l.GetAmount()).Returns(6000).AtMost(1);
+            loan2.Setup(l => l.SaveStatus("Critical", null)).AtMost(1);
 
-            new SimpleSlice().EscalateProblems(new Loan[] { loan1.Object }, null);
+            new SimpleSlice().EscalateProblems(new Loan[] { loan1.Object, loan2.Object}, null);
         }
     }
 }
