78ef2335b6afae969a4e2deb56614164887960ff
jason-kerney <jason@teamkerney.com>
2022-12-02 16:59:29 -0800

    e removed required project
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

---
+++ Talk.Tests/ConnectionTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Talk.Required;
+
+namespace Talk.Tests
+{
+    [TestClass]
+    public class ConnectionTest
+    {
+        [TestMethod]
+        public void TestConnections()
+        {
+            Loan l = null;
+        }
+    }
+}
