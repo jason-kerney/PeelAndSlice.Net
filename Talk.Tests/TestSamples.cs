29caa23f23a12d019edf89f01cc6050671c4382e
jason-kerney <jason@teamkerney.com>
2022-12-02 16:52:55 -0800

    t started solution
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- Talk.Tests/TestSamples.cs
+++ Talk.Tests/TestSamples.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Talk.Required;
 
 namespace Talk.Tests
 {
@@ -9,9 +11,16 @@ namespace Talk.Tests
         public void TestSimplePeel()
         {
         }
+
         [TestMethod]
         public void TestSimpleSlice()
         {
+            var loan1 = new Mock<Loan>(MockBehavior.Strict);
+
+            loan1.Setup(l => l.IsOverDue()).Returns(true);
+            loan1.Setup(l => l.GetAmount()).Returns(5);
+
+            new SimpleSlice().EscalateProblems(new Loan[] { loan1.Object }, null);
         }
     }
 }
